07ec7a928e0c1c886d973e78bbdff92d6932296b
zhayneq <159972216+zhayneq@users.noreply.github.com>
2024-02-15 10:09:14 -0600

    ndex.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'playing',
+    status: 'dnd',
   });
 
   
