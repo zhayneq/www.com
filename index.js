75108e53e8685d7bbe97d5802a0083a1fee73329
zhayneq <159972216+zhayneq@users.noreply.github.com>
2024-02-17 23:37:04 -0600

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ['gta 5'];
+const statusMessages = ['truth or dare '];
 
 
 let currentIndex = 0;
@@ -74,7 +74,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'online',
+    status: 'dnd',
   });
 
   
